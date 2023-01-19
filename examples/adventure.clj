38b0d2e07e5e0506642de349ad0e52637a38053b
Stathis Sideris <stathis@pixelated-noise.com>
2023-01-19 16:16:56 +0200

    Explain that cancel is not being handled in adventure.clj

--- examples/adventure.clj
+++ examples/adventure.clj
@@ -13,17 +13,20 @@
 (message "Our story"
          "You are a little panda who lives in a little panda village
           in a little bamboo grove in the highlands. It is a scary
-          time for the little village. The dark lord's armies have 
+          time for the little village. The dark lord's armies have
           been sweeping the lands below, and the people fear that
           soon he will come for the little panda village too. You
           have been chosen to go forth and investigate.")
 
+;; the code below does not account for the player hitting "cancel" in which case
+;; we get nil values
+
 (loop []
   (swap! game-state assoc :cname (input "What is your name, little one?"
-                                       "Enter your name below."))
+                                        "Enter your name below."))
 
   (swap! game-state assoc :class (radiolist "What is your class?"
-                                            "What has your panda trained as? 
+                                            "What has your panda trained as?
                                            Choose below."
                                             [[:WAR "Warrior" true]
                                              [:MGE "Mage" false]
@@ -35,7 +38,7 @@
     :ROG (add-item! :knife))
 
   (add-item! :potion)
-  
+
   (when-not (confirm "Are you happy with these choices?"
                      (str/join "\n"
                                (let [{:keys [cname class items]} @game-state]
@@ -48,8 +51,8 @@
 (def choice1
   (menu "The adventure begins"
         (str "Alright, " (:cname @game-state) ", time to set off."
-             "You make your way down the path through the grove and reach"
-             "a fork in the road. Which way will you go?")
+             " You make your way down the path through the grove and reach"
+             " a fork in the road. Which way will you go?")
         {:W "West"
          :E "East"}))
 
@@ -59,4 +62,3 @@
                 "and slide down the hill, only to find the bridge is out! The little panda tumbles to their doom "
                 "in the river below, never to be seen again."))
   (System/exit 0))
-
